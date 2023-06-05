322fed00644e1282dbc063dcb0203716da99295f
shais <muhammadshais333@gmail.com>
2023-06-05 11:03:15 -0700

    developed welcome page

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+class Home extends React.Component{
+    render(){
+       return(
+        <div className='footer'>
+        </div>
+       )
+    
+    }
+}
+export default Home
